b4f231fbb9284a288f2a5fce83e3410383f690f6
evan-carlsen <evan@crowdbotics.com>
2023-03-02 13:29:36 -0800

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- screens/Untitled1/index.js
+++ screens/Untitled1/index.js
@@ -1,20 +1,33 @@
+import { Pressable } from "react-native";
+import { View } from "react-native";
 import React from "react";
 import { StyleSheet, ScrollView, SafeAreaView } from "react-native";
 
-const GettingStarted = () => {
+const GettingStarted = ({
+  navigation
+}) => {
   return <SafeAreaView style={styles.safeArea}>
         <ScrollView contentContainerStyle={{
       backgroundColor: '#f0f0f1',
       padding: 10,
       position: 'relative',
       flex: 1
-    }}></ScrollView>
+    }}><Pressable onPress={{}}><Pressable onPress={() => {
+          navigation.navigate("Untitled5");
+        }}><View style={styles.rSmptqdo}></View></Pressable></Pressable></ScrollView>
     </SafeAreaView>;
 };
 
 const styles = StyleSheet.create({
   safeArea: {
     height: '100%'
+  },
+  rSmptqdo: {
+    height: 60,
+    width: 140,
+    backgroundColor: "#E4E4E4",
+    borderRadius: 0,
+    color: "#777777"
   }
 });
 export default GettingStarted;
