028a11db3383217143cd695eb822d7439c34c2b2
evan-carlsen <evan@crowdbotics.com>
2023-03-03 00:07:45 -0800

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- screens/signup/index.js
+++ screens/signup/index.js
@@ -144,16 +144,16 @@ export default Signup;
 
 const Button = props => {
   return <TouchableHighlight onPress={props.onPress} underlayColor='#DDDDDD'>
-      <View style={[btnStyles.button, {
-      backgroundColor: props.backgroundColor ? props.backgroundColor : "#000000",
-      height: props.height ? props.height : 49,
-      borderWidth: props.borderWidth ? props.borderWidth : 0,
-      borderColor: props.borderColor ? props.borderColor : "#000000"
-    }]}>
+      <Pressable onPress={{}}><Pressable onPress={{}}><View style={[btnStyles.button, {
+          backgroundColor: props.backgroundColor ? props.backgroundColor : "#000000",
+          height: props.height ? props.height : 49,
+          borderWidth: props.borderWidth ? props.borderWidth : 0,
+          borderColor: props.borderColor ? props.borderColor : "#000000"
+        }]}>
         <Text style={[btnStyles.text, {
-        color: props.color ? props.color : "#ffffff"
-      }]}>{props.children}</Text>
-      </View>
+            color: props.color ? props.color : "#ffffff"
+          }]}>{props.children}</Text>
+      </View></Pressable></Pressable>
     </TouchableHighlight>;
 };
 
