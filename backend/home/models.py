e1cd19f89c03403d237a997670c4a38112abd429
crowdbotics-dev <engineering@crowdbotics.com>
2023-03-24 18:33:57 +0000

    Update models

--- backend/home/models.py
+++ backend/home/models.py
@@ -3,3 +3,7 @@ from django.db import models
 class Recipe(models.Model):
     'Generated Model'
     title = models.CharField(max_length=256,)
+    instructions = models.TextField(null=True,blank=True,)
+    cook_time = models.IntegerField(null=True,blank=True,)
+    prep_time = models.IntegerField(null=True,blank=True,)
+    chef = models.ForeignKey("users.User",on_delete=models.CASCADE,null=True,blank=True,related_name="recipe_chef",)
