8d7b4e114629a2d9e65d6ca2a3d4c13deb2d373a
saad-abid-crowdbotics <saad.abid@crowdbotics.com>
2023-04-04 11:15:46 +0500

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

---
+++ screens/Untitled12/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { StyleSheet, SafeAreaView } from "react-native";
+
+const Untitled12 = () => {
+  return <SafeAreaView style={styles.safeArea}>
+        
+    </SafeAreaView>;
+};
+
+const styles = StyleSheet.create({
+  safeArea: {
+    height: '100%',
+    backgroundColor: "#ea1313"
+  }
+});
+export default Untitled12;
