c58a786f62dd7c1a83f0f661160d987d8fa82a6a
saad-abid-crowdbotics <saad.abid@crowdbotics.com>
2023-04-10 16:49:24 +0500

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- screens/Untitled1/index.js
+++ screens/Untitled1/index.js
@@ -1,20 +1,177 @@
+import { ImageBackground } from "react-native";
+import { View } from "react-native";
+import { Text } from "react-native";
 import React from "react";
-import { StyleSheet, ScrollView, SafeAreaView } from "react-native";
+import { StyleSheet, SafeAreaView } from "react-native";
 
 const Untitled1 = () => {
   return <SafeAreaView style={styles.safeArea}>
-        <ScrollView contentContainerStyle={{
-      backgroundColor: '#f0f0f1',
-      padding: 10,
-      position: 'relative',
-      flex: 1
-    }}></ScrollView>
+      <View style={styles.TJoFcOio}>
+        <View style={styles.wXultJUT}><View style={styles.gWGaGXnx}><View style={styles.hJfASQBN}><View style={styles.vmVkOoEm}><View style={styles.DkXMqeVm}><View style={styles.OVBXxATX}><View style={styles.QGKHexBY}><View style={styles.bnoVXtjy}><Text style={styles.bWcSoNgt}>Instagram</Text></View></View></View></View></View></View></View></View>
+        <View style={styles.kungGegN}><View style={styles.gayZEWHm}><View style={styles.zaERkGdo}><View style={styles.QlUTulsW}><View style={styles.QFaNZKjv}><View style={styles.GOOuuDWh}><View style={styles.lSgftOPP}><View style={styles.dkUHzQuo}><View style={styles.fsuvkwJw}>
+          <View style={styles.bWSKjvrR}>
+            <ImageBackground style={styles.YQsvIcbW} source={require("./file.png")} resizeMode="cover"></ImageBackground>
+            <ImageBackground style={styles.HhdseLnU} source={require("./crazy-light.png")} resizeMode="cover"></ImageBackground>
+            <ImageBackground style={styles.rKKqEQWu} source={require("./icons8-image-48.png")} resizeMode="cover"></ImageBackground>
+          </View>
+        </View></View></View></View></View></View></View></View></View>
+      </View>
     </SafeAreaView>;
 };
 
 const styles = StyleSheet.create({
-  safeArea: {
-    height: '100%'
+  bWcSoNgt: {
+    width: 110,
+    height: 17,
+    lineHeight: 14,
+    fontSize: 22,
+    borderRadius: 0,
+    fontWeight: "700",
+    textAlign: "left"
+  },
+  TJoFcOio: {
+    height: 44,
+    width: "100%",
+    backgroundColor: "#fffcfc",
+    borderRadius: 0,
+    color: "#777777",
+    position: "absolute",
+    top: 0,
+    alignItems: "center",
+    flexWrap: "no-wrap",
+    flexDirection: "row",
+    flex: 1,
+    left: 0,
+    transform: [{
+      rotate: "0deg"
+    }]
+  },
+  nlqaVhnv: {
+    width: 100,
+    height: 50,
+    lineHeight: 14,
+    fontSize: 14,
+    borderRadius: 0
+  },
+  EGJTTpOl: {
+    width: 110,
+    height: 17,
+    lineHeight: 14,
+    fontSize: 22,
+    borderRadius: 0,
+    fontWeight: "700",
+    textAlign: "left"
+  },
+  QKfwoaAn: {
+    width: 110,
+    height: 17,
+    lineHeight: 14,
+    fontSize: 22,
+    borderRadius: 0,
+    fontWeight: "700",
+    textAlign: "left"
+  },
+  SXASVkUo: {
+    flex: 1
+  },
+  LCOMVMEQ: {
+    flex: 1,
+    backgroundColor: "#ffffff"
+  },
+  gwvdVPnn: {
+    width: 100,
+    height: 50,
+    lineHeight: 14,
+    fontSize: 14,
+    borderRadius: 0
+  },
+  bWSKjvrR: {
+    height: "100%",
+    width: "30%",
+    backgroundColor: "#ffffff",
+    borderRadius: 0,
+    color: "#777777",
+    flexDirection: "row",
+    flex: 1,
+    justifyContent: "space-evenly",
+    alignItems: "center",
+    flexWrap: "no-wrap"
+  },
+  YQsvIcbW: {
+    width: 20,
+    height: 20
+  },
+  CsOfyfea: {
+    flex: 1,
+    width: 0
+  },
+  HhdseLnU: {
+    width: 20,
+    height: 20
+  },
+  rKKqEQWu: {
+    width: 20,
+    height: 20
+  },
+  hwoXoAOM: {
+    flex: 1
+  },
+  QWRetKXI: {
+    flex: 1
+  },
+  HBtqRemI: {
+    flex: 1
+  },
+  fsuvkwJw: {
+    flex: 1
+  },
+  bnoVXtjy: {
+    flex: 1
+  },
+  dkUHzQuo: {
+    flex: 1
+  },
+  QGKHexBY: {
+    flex: 1
+  },
+  lSgftOPP: {
+    flex: 1
+  },
+  OVBXxATX: {
+    flex: 1
+  },
+  GOOuuDWh: {
+    flex: 1
+  },
+  DkXMqeVm: {
+    flex: 1
+  },
+  QFaNZKjv: {
+    flex: 1
+  },
+  vmVkOoEm: {
+    flex: 1
+  },
+  QlUTulsW: {
+    flex: 1
+  },
+  hJfASQBN: {
+    flex: 1
+  },
+  zaERkGdo: {
+    flex: 1
+  },
+  gWGaGXnx: {
+    flex: 1
+  },
+  gayZEWHm: {
+    flex: 1
+  },
+  wXultJUT: {
+    flex: 1
+  },
+  kungGegN: {
+    flex: 1
   }
 });
 export default Untitled1;
