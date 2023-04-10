1522f1bebee1871feb2067abd52bf7d7e32b9b1b
saad-abid-crowdbotics <saad.abid@crowdbotics.com>
2023-04-10 15:19:41 +0500

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- screens/Untitled11/index.js
+++ screens/Untitled11/index.js
@@ -1,3 +1,4 @@
+import { TextInput } from "react-native";
 import { Text } from "react-native";
 import React from "react";
 import { StyleSheet, ScrollView, SafeAreaView } from "react-native";
@@ -9,7 +10,7 @@ const Untitled11 = () => {
       padding: 10,
       position: 'relative',
       flex: 1
-    }}><Text style={styles.uHTAzksn}>Lorem ipsum…</Text></ScrollView>
+    }}><Text style={styles.uHTAzksn}>Lorem ipsum…</Text><TextInput style={styles.RFhkuTOP}></TextInput></ScrollView>
     </SafeAreaView>;
 };
 
@@ -23,6 +24,12 @@ const styles = StyleSheet.create({
     lineHeight: 14,
     fontSize: 14,
     borderRadius: 0
+  },
+  RFhkuTOP: {
+    backgroundColor: "#ffffff",
+    borderColor: "#cccccc",
+    width: 150,
+    height: 30
   }
 });
 export default Untitled11;
