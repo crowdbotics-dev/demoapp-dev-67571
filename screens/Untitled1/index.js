188d02d7d4d606ccc4459992282740dd29105276
saad-abid-crowdbotics <saad.abid@crowdbotics.com>
2023-04-10 17:10:58 +0500

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- screens/Untitled1/index.js
+++ screens/Untitled1/index.js
@@ -8,11 +8,11 @@ const Untitled1 = () => {
   return <SafeAreaView style={styles.safeArea}>
       <View style={styles.TJoFcOio}>
         <Text style={styles.bWcSoNgt}>Instagram</Text>
-          <View style={styles.MZTtIvnO}>
-            <ImageBackground style={styles.YQsvIcbW} source={require("./file.png")} resizeMode="cover"></ImageBackground>
-            <ImageBackground style={styles.HhdseLnU} source={require("./crazy-light.png")} resizeMode="cover"></ImageBackground>
-            <ImageBackground style={styles.rKKqEQWu} source={require("./icons8-image-48.png")} resizeMode="cover"></ImageBackground>
-          </View>
+        <View style={styles.MZTtIvnO}>
+          <ImageBackground style={styles.YQsvIcbW} source={require("./file.png")} resizeMode="cover"></ImageBackground>
+          <ImageBackground style={styles.HhdseLnU} source={require("./crazy-light.png")} resizeMode="cover"></ImageBackground>
+          <ImageBackground style={styles.rKKqEQWu} source={require("./icons8-image-48.png")} resizeMode="cover"></ImageBackground>
+        </View>
       </View>
     </SafeAreaView>;
 };
@@ -51,38 +51,6 @@ const styles = StyleSheet.create({
     fontSize: 14,
     borderRadius: 0
   },
-  EGJTTpOl: {
-    width: 110,
-    height: 17,
-    lineHeight: 14,
-    fontSize: 22,
-    borderRadius: 0,
-    fontWeight: "700",
-    textAlign: "left"
-  },
-  QKfwoaAn: {
-    width: 110,
-    height: 17,
-    lineHeight: 14,
-    fontSize: 22,
-    borderRadius: 0,
-    fontWeight: "700",
-    textAlign: "left"
-  },
-  SXASVkUo: {
-    flex: 1
-  },
-  LCOMVMEQ: {
-    flex: 1,
-    backgroundColor: "#ffffff"
-  },
-  gwvdVPnn: {
-    width: 100,
-    height: 50,
-    lineHeight: 14,
-    fontSize: 14,
-    borderRadius: 0
-  },
   bWSKjvrR: {
     height: "100%",
     width: "30%",
@@ -99,140 +67,67 @@ const styles = StyleSheet.create({
     width: 20,
     height: 20
   },
-  CsOfyfea: {
-    flex: 1,
-    width: 0
-  },
-  HhdseLnU: {
-    width: 20,
-    height: 20
-  },
-  rKKqEQWu: {
-    width: 20,
-    height: 20
-  },
-  hwoXoAOM: {
+  KXeFFQmO: {
     flex: 1
   },
-  QWRetKXI: {
+  iBusSrqJ: {
     flex: 1
   },
-  HBtqRemI: {
+  RxBkzlKR: {
     flex: 1
   },
-  fsuvkwJw: {
+  CeDeEBLr: {
     flex: 1
   },
-  bnoVXtjy: {
+  EJtBhXJs: {
     flex: 1
   },
-  dkUHzQuo: {
+  UdiNvgAJ: {
     flex: 1
   },
-  QGKHexBY: {
+  FEjDYdYX: {
     flex: 1
   },
-  lSgftOPP: {
+  cbCFuzMi: {
     flex: 1
   },
-  OVBXxATX: {
+  DQEfbQAk: {
     flex: 1
   },
-  GOOuuDWh: {
+  dLBNAjNb: {
     flex: 1
   },
-  DkXMqeVm: {
+  aKZPDsZY: {
     flex: 1
   },
-  QFaNZKjv: {
+  RDCBoahs: {
     flex: 1
   },
-  vmVkOoEm: {
+  lPgCwMjj: {
     flex: 1
   },
-  QlUTulsW: {
+  sPKWEWBZ: {
     flex: 1
   },
-  hJfASQBN: {
+  DeUqUeLv: {
     flex: 1
   },
-  zaERkGdo: {
+  kUdnoqPi: {
     flex: 1
   },
-  gWGaGXnx: {
+  IpRnkuqt: {
     flex: 1
   },
-  gayZEWHm: {
+  pFPuVJll: {
     flex: 1
   },
-  wXultJUT: {
+  kGaizgwY: {
     flex: 1
   },
-  kungGegN: {
+  ASbctzXh: {
     flex: 1
   },
-  HLgzCBNt: {
-    flex: 1
-  },
-  RmsMDeRW: {
-    flex: 1
-  },
-  rhsDsWsQ: {
-    flex: 1
-  },
-  LLmNMuJZ: {
-    flex: 1
-  },
-  lqToMnnf: {
-    flex: 1
-  },
-  EOeoEBoq: {
-    flex: 1
-  },
-  UIWdBlUh: {
-    flex: 1
-  },
-  XLQJQRVa: {
-    flex: 1
-  },
-  fPYgQfKs: {
-    flex: 1
-  },
-  QrdJcHYf: {
-    flex: 1
-  },
-  iHNCmSyz: {
-    flex: 1
-  },
-  BELFNGoS: {
-    flex: 1
-  },
-  WSdTNwRy: {
-    flex: 1
-  },
-  DSvSTXJR: {
-    flex: 1
-  },
-  pyHqaofH: {
-    flex: 1
-  },
-  MZTtIvnO: {
-    flex: 1,
-    flexDirection: "row"
-  },
-  fuuQMHyi: {
-    flex: 1
-  },
-  WCnlbHSL: {
-    flex: 1
-  },
-  BWdowGrt: {
-    flex: 1
-  },
-  uBDaZAHn: {
-    flex: 1
-  },
-  ndZowlYB: {
+  envoBwRb: {
     flex: 1
   }
 });
